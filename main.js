87c9a645737d6818c1b8af8c6db751c578a1a436
julian21olarte <julian21olarte@gmail.com>
2018-04-12 09:09:47 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+});
+
+function loadExamples(event) {
+  event.preventDefault();
+  $('.content').load('hola');
+}
+function loadExcercises(event) {
+  event.preventDefault();
+  $('.content').load('hola');
+}
